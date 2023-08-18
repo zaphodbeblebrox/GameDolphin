fdb692a7969b869daa4f5fdb3ebe2f7b2e6bc106
Brandon Withrow <withrobm@gmail.com>
2023-08-17 20:46:22 -0700

    Changed platform to [platform]
    This makes it expect an array of strings. Due to settings on my pc, prettier auto-formatted everything on save.

--- server/models/cheatCode.model.js
+++ server/models/cheatCode.model.js
@@ -1,39 +1,38 @@
-const mongoose = require('mongoose');
-const GameDolphineSchema = new mongoose.Schema({
+const mongoose = require("mongoose");
+const GameDolphineSchema = new mongoose.Schema(
+    {
+        //cheat code model
 
-    //cheat code model
+        game: {
+            type: String,
+            required: [true, "Game name is required"],
+            trim: true,
+            minlength: [2, "Game name must be at least 2 characters long!"],
+        },
 
-    game: {
-        type: String, 
-        required: [ true, "Game name is required" ],
-        trim: true,
-        minlength: [2, "Game name must be at least 2 characters long!"]
+        description: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: [3, "Game description cannot be empty!"],
+            maxlength: [255, "Game description cannot exceed 255 characters!"],
+        },
+
+        instructions: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: [3, "Instructions cannot be blank!"],
+        },
+
+        platform: {
+            type: [String],
+            required: true,
+            trim: true,
+            minlength: [1, "Platform/console must be added"],
+        },
     },
+    { timestamps: true }
+);
 
-    description: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: [3, "Game description cannot be empty!"],
-        maxlength: [255, "Game description cannot exceed 255 characters!"]
-    },
-
-    instructions: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: [3, "Instructions cannot be blank!"]
-    },
-
-    platform: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: [1, "Platform/console must be added"]
-    }
-    
-}, { timestamps: true });
-
-module.exports = mongoose.model('CheatCode', GameDolphineSchema);
-
-
+module.exports = mongoose.model("CheatCode", GameDolphineSchema);
