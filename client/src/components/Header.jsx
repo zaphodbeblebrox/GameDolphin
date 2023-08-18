86f2d6aca55d55be18d3dd601b94d483ec97c929
Brandon Withrow <withrobm@gmail.com>
2023-08-17 17:20:36 -0700

    Updated Header with conditional Home button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = (props) => {
+    const location = useLocation();
+
     return (
         <div>
             <h1>Game Dolphin</h1>
             <h4>Your One Stop for Cheats for Classic Video Games!</h4>
+            {location.pathname !== "/gamedolphin/dashboard" && <Link to={"/gamedolphin/dashboard"}>Home</Link>}
         </div>
     );
 };
