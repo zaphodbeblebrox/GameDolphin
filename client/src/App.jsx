aeb6e29b968f7babfc6b7037b44fc2de9b4acc2b
Brandon Withrow <withrobm@gmail.com>
2023-08-17 17:14:40 -0700

    got barebones dashboard displaying

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import EditCreateCheat from "./components/EditCreateCheat";
+import Game from "./components/Game";
+import Header from "./components/Header";
+import { cheatApi } from "./services/apiAddress";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [cheats, setCheats] = useState([
+        { game: "minecraft", _id: "3asd4f351fsa63" },
+        { game: "dark Souls", _id: "s165gf1sdf651d" },
+    ]);
+
+    // TODO: axios in DB
+    useEffect(() => {
+        // axios
+        //     .get(`${cheatApi}`)
+        //     .then((res) => {
+        //         const sortedData = res.data.sort((a, b) => a.game.localeCompare(b.game));
+        //         setCheats(sortedData);
+        //     })
+        //     .catch((err) => console.log(err));
+
+        const sortedData = cheats.sort((a, b) => a.game.localeCompare(b.game));
+
+        setCheats([...sortedData]);
+    }, []);
+
+    return (
+        <BrowserRouter Router>
+            <div className="App">
+                <div>
+                    <Header />
+                </div>
+                <Routes>
+                    <Route path="/" element={<Navigate to="/gamedolphin/dashboard" />} />
+                    <Route path="/gamedolphin/dashboard" element={<Dashboard cheats={cheats} />} />
+                    <Route path="/gamedolphin/create" element={<EditCreateCheat cheats={cheats} setCheats={setCheats} />} />
+                    <Route path="/gamedolphin/update/:cheatId" element={<EditCreateCheat cheats={cheats} setCheats={setCheats} />} />
+                    <Route path="/gamedolphin/:gameId" element={<Game cheats={cheats} setCheats={setCheats} />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = (props) => {
+    return (
+        <div>
+            <h1>Game Dolphin</h1>
+            <h4>Your One Stop for Cheats for Classic Video Games!</h4>
+        </div>
+    );
+};
+
+export default Header;
